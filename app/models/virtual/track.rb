40f706364b44211904726c3b6e2cd0467a338c75
Matej Kováč <kovo.mato@gmail.com>
2021-01-23 12:46:09 +0100

    repairing bug app crash when parsing songs shorther than minute

--- app/models/virtual/track.rb
+++ app/models/virtual/track.rb
@@ -17,7 +17,7 @@ class Virtual::Track
     def self.processTracks(tracks)
         result = []
         url_regex = /^(http\:\/\/|https\:\/\/)?(www)?(bandzone\.cz\/track\/)(play)(\/)(\d+)(.+)$/
-        duration_regex = /^PT(\d+)M(\d*\.*\d*)S*/
+        duration_regex = /^PT(\d*)M?(\d*\.*\d*)S*/
         album_regex = /\s*\-*\s*/
         plays_count_regex = /\D/i
 
@@ -27,8 +27,13 @@ class Virtual::Track
             title = ""
             href = track['data-source'].sub(url_regex, "\\1\\2\\3download\\5\\6")
             duration_match = track.css("meta[itemprop=duration]")[0]['content'].match(duration_regex)
-            duration_minutes = duration_match[1].to_s
-            duration_seconds = duration_match[2].to_f.floor.to_s
+            if duration_match[2].blank?
+                duration_seconds = duration_match[1]
+                duration_minutes = "00"
+            else
+                duration_minutes = duration_match[1].to_s
+                duration_seconds = duration_match[2].to_f.floor.to_s
+            end
             duration_seconds = duration_seconds.length <= 1 ? "0#{duration_seconds}" : duration_seconds
             duration = "#{duration_minutes}:#{duration_seconds}"
 
